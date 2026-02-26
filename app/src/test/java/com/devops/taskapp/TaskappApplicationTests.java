b4a67af9058fc1830f9af013049288798f176bf9
Pujari Hiranmayee Panda <ppanda@nisum.com>
2026-02-26 11:53:22 +0530

    Added Spring Boot base project

---
+++ app/src/test/java/com/devops/taskapp/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
